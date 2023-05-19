7b3eb9a0313c10340a5a0bef5fc76696f94da5a1
Aditya Vikram Singh <adityas.1731@gmail.com>
2023-05-19 19:20:20 +0530

    initial commit

---
+++ src/components/Data.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+const Data = () => {
+
+    const [data,setData] = useState([]);
+
+    const fetchData = async () => {
+        
+        const response = await axios.get('https://raw.githubusercontent.com/dixitsoham7/dixitsoham7.github.io/main/index.json')
+
+        setData(response.data.employees)
+        console.log(data);
+    }
+
+    useEffect(()=>{
+        fetchData();
+    })
+  return (
+<div class="flex flex-col m-16">
+  <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+      <div class="overflow-hidden">
+        <table class="min-w-full text-left text-sm font-light">
+          <thead class="border-b font-medium dark:border-neutral-500">
+            <tr>
+              <th scope="col" class="px-6 py-4">ID</th>
+              <th scope="col" class="px-6 py-4">Name</th>
+              <th scope="col" class="px-6 py-4">Skills</th>
+              <th scope="col" class="px-6 py-4">Designation</th>
+              <th scope="col" class="px-6 py-4">Projects</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                data.length > 0 && 
+                data.map((item)=>{
+                    return (
+                        <tr
+              class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+              <td class="whitespace-nowrap px-6 py-4 font-medium">{item.id}</td>
+              <td class="whitespace-nowrap px-6 py-4">{item.name}</td>
+              <td class="whitespace-nowrap px-6 py-4">{item.skills.map((e)=>{
+                return(
+                    <p>{e}</p>
+                )
+              })}</td>
+              <td class="whitespace-nowrap px-6 py-4">{item.designation}</td>
+              {
+                item.projects && <td class="whitespace-nowrap px-6 py-4">{item.projects.map((e)=>{
+                    return(
+                        <>
+                        <b className='font-bold'>{e.name}</b>
+                        <p>{e.description}</p>
+                        <br/>
+                        <p className='font-bold'>TEAMS</p>
+                        <p>{e.team.map((e)=>{
+                            return (
+                                <>
+                                <p>Name : {e.name}</p>
+                                <p>Role : {e.role}</p><br/>
+                                </>
+                            )
+                        })}</p>
+                        {
+                            e.tasks && <p>{e.tasks.map((e)=>{
+                                return (
+                                    <>
+                                    <p>ID : {e.id}</p>
+                                    <p>Name : {e.name}</p>
+                                    <p>Status : {e.status}</p><br/>
+                                    </>
+                                )
+                            })}</p>
+                        }
+    
+                        </>
+                    )
+                  })}</td>
+              }
+            </tr>
+                    )
+                })
+            }
+            
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default Data
